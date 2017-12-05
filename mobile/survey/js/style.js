f30e6cbad63b5c9e65874b29e0ec729264a1cf23
gaishou <gaishou@foxmail.com>
2017-12-05 10:58:11 +0800

    静态

---
+++ mobile/survey/js/style.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#stop").mouseover(function(){
+        $("#stop").css("src ","img/stop_sel@2x.png3");
+    });
+    $("#module1").mouseout(function(){
+        $("#module1").css("border","1px solid #cccccc");
+    });
