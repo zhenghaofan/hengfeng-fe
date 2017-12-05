a597ae410a6c2a81ba488cdbf6bcd47269392cca
Goblin <991012287@qq.com>
2017-12-05 11:17:16 +0800

    change

--- mobile/survey/js/style.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("#stop").mouseover(function(){
-        $("#stop").css("src ","img/stop_sel@2x.png3");
-    });
-    $("#module1").mouseout(function(){
-        $("#module1").css("border","1px solid #cccccc");
-    });
