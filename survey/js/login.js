eb3af612980399f39d0c2f689582843db74c3ab4
zhenghaofan <357034385@qq.com>
2017-12-06 14:05:56 +0800

    fix login

--- survey/js/login.js
+++ survey/js/login.js
@@ -1,23 +1,27 @@
-$("#login_bt").click(function () {
-    const username = $("#login_name").val();
-    const pwd = $("#login_word").val();
+$(document).ready(function() {
+  let url = '/login';
+  $('#login_bt').click(function() {
+    let username = $('#login_name').val();
+    let password = $('#login_word').val();
+    if (username === '' || password === '') {
+      $("#err_tips").html("请输入正确的账号和密码");
+      return;
+    }
 
-    $.post("/manage/account/user/preview/{{userId}}",{
+    $.ajax({
+      url: url,
+      data: {
         username: username,
-        pwd: pwd
-    },function (data, message) {
-        if(username === "" && pwd === ""){//判断输入框是否为空值
-            $("#err_tips").text("*请输入正确的账号和密码");
-        }else {
-            if(username.message === "false"){//判断账号是否正确
-                if(pwd.message === "false"){//判断密码是否正确
-                    window.location.href="main.html";//账号密码都正确，就跳转到首页
-                }else {
-                    $("#err_tips").text("*请输入正确的密码");
-                }
-            }else {
-                $("#err_tips").text("*请输入正确的账号");
-            }
+        password: password,
+      },
+      type: 'POST',
+      success: function(data) {
+        if (data.resultCode != 'SUCCESS') {
+            $("#err_tips").html(data.message);
+        } else {
+          window.location.href = 'main.html'
         }
+      }
     })
-});
+  })
+})
