d49b38ceb2377636d8d21e4c086b1dba2e95abbd
zhenghaofan <357034385@qq.com>
2017-12-06 14:12:21 +0800

    fix

--- survey/js/login.js
+++ survey/js/login.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   let url = '/login';
-  $('#login_bt').click(function() {
-    let username = $('#login_name').val();
-    let password = $('#login_word').val();
+  $('.login_bt').click(function() {
+    let username = $('.login_name').val();
+    let password = $('.login_word').val();
     if (username === '' || password === '') {
-      $("#err_tips").html("请输入正确的账号和密码");
+      $(".err_tips").html("请输入正确的账号和密码");
       return;
     }
 
@@ -17,7 +17,7 @@ $(document).ready(function() {
       type: 'POST',
       success: function(data) {
         if (data.resultCode != 'SUCCESS') {
-            $("#err_tips").html(data.message);
+            $(".err_tips").html(data.message);
         } else {
           window.location.href = 'main.html'
         }
